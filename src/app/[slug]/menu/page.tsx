d80e50dac723d067e30d4c2b01c7832a241ea225
francisco.bruno@armateus.com.br <bruno-amil@hotmail.com>
2025-02-23 08:58:33 -0300

    feat: create page for restaurant

---
+++ src/app/[slug]/menu/page.tsx
@@ -0,0 +1,42 @@
+import { ChevronLeftIcon, ScrollTextIcon } from "lucide-react";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/prisma";
+
+import { RestaurantHeader } from "./components/header";
+
+interface RestaurantMenuPageProps {
+  params: Promise<{ slug: string }>;
+  searchParams: Promise<{ consumptionMethod: string }>;
+}
+const isConsumptionMethodValid = (consumptionMethod: string) => {
+  return ["DINE_IN","TAKEAWAY"].includes(consumptionMethod?.toLowerCase());
+};
+const RestaurantMenuPage = async ({
+  params,
+  searchParams,
+}: RestaurantMenuPageProps) => {
+  const { slug } = await params;
+  const { consumptionMethod } = await searchParams;
+
+  console.log("consumptionMethod", consumptionMethod)
+  if (isConsumptionMethodValid(consumptionMethod)) {
+    return notFound();
+  }
+
+  const restaurant = await db.restaurant.findUnique({ where: { slug } });
+
+  if (!restaurant){
+    notFound();
+  }
+
+  return (
+    <div>
+      <RestaurantHeader restaurant={restaurant}/>
+    </div>
+  );
+};
+
+export default RestaurantMenuPage;
--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,11 @@
+import Image from "next/image";
+import { notFound } from "next/navigation";
+
 import { db } from "@/lib/prisma";
+
+import { ConsumptionMethodOption } from "./components/consumption-option";
+
+
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -6,7 +13,37 @@ interface RestaurantPageProps {
 const Restaurantpage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
   const restaurant = await db.restaurant.findUnique({ where: { slug } });
-  return <h2>{restaurant?.name}</h2>;
+
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center px-6 pt-24">
+      <div>
+        <div className="flex flex-col items-center gap-2">
+          <Image
+            src={restaurant.avatarImageUrl}
+            alt={restaurant.name}
+            height={82}
+            width={82}
+          />
+          <h2 className="font-semibold">{restaurant.name} </h2>
+        </div>
+        <div className="space-y-2 pt-24 text-center">
+          <h3 className="text-2xl font-semibold">Seja Bem Vindo</h3>
+          <p className="opacity-55">
+            Escolha como prefere aproveitar a sua refeição Estamos a oferecer
+            praticidade e saboer em cada detalhe.
+          </p>
+        </div>
+        <div className="grid grid-cols-2 gap-4 pt-14">
+          <ConsumptionMethodOption  slug={slug} buttonText="Comer aqui" imageAlt="Comer aqui" imageUrl="/dine_in.png" option="DINE_IN"/>
+          <ConsumptionMethodOption slug={slug} buttonText="Para levar" imageAlt="Para levar" imageUrl="/takeaway.png" option="TAKEAWAY"/>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Restaurantpage;
